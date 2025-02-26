4b2dfa41b17f68f19b3c8ef461bce42abf3593cf
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2025-02-26 20:23:08 +0530

    Mobile Nav and Hero 👍

--- memo/app/components/Navbar.jsx
+++ memo/app/components/Navbar.jsx
@@ -1,7 +1,11 @@
 'use client'
-import React from 'react'
+import { AlignRight } from 'lucide-react';
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [navBar, setNavBar] = useState(false);
+
+
   const handleScroll = (e) => {
     e.preventDefault();
     const targetId = e.currentTarget.getAttribute("href").slice(1);
@@ -15,25 +19,51 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex flex-row py-8 px-32 justify-between items-center fixed top-0 w-full backdrop-blur-md z-40 bg-white/70'>
-      <div>
-        <a href="#Hero" onClick={handleScroll}><img src="Logo.png" className='w-40' alt="" /></a>
+    <div>
+      <div className=' hidden lg:flex flex-row py-8 px-32 justify-between items-center fixed top-0 w-full backdrop-blur-md z-40 bg-white/70'>
+        <div>
+          <a href="#Hero" onClick={handleScroll}><img src="Logo.png" className='w-40' alt="" /></a>
+        </div>
+        <div>
+          <ul className='flex space-x-10 text-memop'>
+            <a href="#what" onClick={handleScroll}>
+              <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>what is memo</li>
+            </a>
+            <a href="#features" onClick={handleScroll}>
+              <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>features</li>
+            </a>
+            <a href="#founders" onClick={handleScroll}>
+              <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>meet the founders</li>
+            </a>
+          </ul>
+        </div>
+        <div className=''>
+          <a href="https://forms.gle/P98oou8ZZXNRRBwh9" target='_blank' className='text-memop border-memop border-2 py-3 px-10 rounded-full hover:bg-memop hover:text-white hover:shadow-lg shadow-memop transition-all'>Join the waitlist</a>
+        </div>
       </div>
-      <div>
-        <ul className='flex space-x-10 text-memop'>
-          <a href="#what" onClick={handleScroll}>
-            <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>what is memo</li>
-          </a>
-          <a href="#features" onClick={handleScroll}>
-            <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>features</li>
-          </a>
-          <a href="#founders" onClick={handleScroll}>
-            <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>meet the founders</li>
-          </a>
-        </ul>
+      <div className='z-50 lg:hidden fixed top-8 right-5 text-memop'>
+        <AlignRight onClick={() => setNavBar(!navBar)}/>
       </div>
-      <div className=''>
-        <a href="https://forms.gle/P98oou8ZZXNRRBwh9" target='_blank' className='text-memop border-memop border-2 py-3 px-10 rounded-full hover:bg-memop hover:text-white hover:shadow-lg shadow-memop transition-all'>Join the waitlist</a>
+      <div className={`bg-white w-screen h-screen items-center justify-center text-center flex flex-col text-white space-y-20 ${navBar ? 'translate-x-0' : 'translate-x-full hidden'} lg:hidden transition-all`}>
+        <div >
+          <a href="#Hero" onClick={handleScroll}><img src="Logo.png" className='w-40' alt="" /></a>
+        </div>
+        <div className=''>
+          <ul className='flex flex-col space-y-10  text-memop text-2xl'>
+            <a href="#what" onClick={handleScroll}>
+              <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>what is memo</li>
+            </a>
+            <a href="#features" onClick={handleScroll}>
+              <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>features</li>
+            </a>
+            <a href="#founders" onClick={handleScroll}>
+              <li className='cursor-pointer hover:underline transition-all underline-offset-8 hover:text-memob'>meet the founders</li>
+            </a>
+          </ul>
+        </div>
+        <div className=''>
+          <a href="https://forms.gle/P98oou8ZZXNRRBwh9" target='_blank' className='text-memop border-memop border-2 py-3 px-10 rounded-full hover:bg-memop text-xl hover:text-white hover:shadow-lg shadow-memop transition-all'>Join the waitlist</a>
+        </div>
       </div>
     </div>
   )
