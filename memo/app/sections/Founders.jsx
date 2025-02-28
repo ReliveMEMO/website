2ce2a5ccac0cf0097da660e213eaaaf0f6f7fa63
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2025-02-28 13:21:07 +0530

    link updates

--- memo/app/sections/Founders.jsx
+++ memo/app/sections/Founders.jsx
@@ -21,15 +21,15 @@ const Founders = () => {
       </div>
 
       <div ref={ref1} className={`flex lg:flex-row flex-col justify-between lg:space-x-5 space-y-4 items-center lg:mt-16 mt-8 ${inView1 ? 'animate__animated animate__fadeInUp' : ''}`}>
-        <a href="#" className='lg:w-1/3 hover:shadow-lg'><img src="Sandinu.png"  alt="" /></a>
-        <a href="#" className='lg:w-1/3 hover:shadow-lg'><img src="Sachin.png"  alt="" /></a>
-        <a href="#" className='lg:w-1/3 hover:shadow-lg'><img src="Thisas.png"  alt="" /></a>
+        <a href="https://www.linkedin.com/in/sandinu-pinnawala-b85b2b20a/" className='lg:w-1/3 hover:shadow-lg'><img src="Sandinu.png"  alt="" /></a>
+        <a href="https://www.linkedin.com/in/sachin-kulathilaka-064037267/" className='lg:w-1/3 hover:shadow-lg'><img src="Sachin.png"  alt="" /></a>
+        <a href="https://www.linkedin.com/in/thisas-ranchagoda-124b82294/" className='lg:w-1/3 hover:shadow-lg'><img src="Thisas.png"  alt="" /></a>
       </div>
 
       <div ref={ref2} className={`flex lg:flex-row flex-col justify-between lg:space-x-5 space-y-4 items-center lg:mt-16 mt-5 ${inView2 ? 'animate__animated animate__fadeInUp animate__delay-1s' : ''}`}>
-        <a href="#" className='lg:w-1/3 hover:shadow-lg'><img src="Malsha.png"  alt="" /></a>
-        <a href="#" className='lg:w-1/3 hover:shadow-lg'><img src="Monali.png"  alt="" /></a>
-        <a href="#" className='lg:w-1/3 hover:shadow-lg'><img src="Sadinsa.png"  alt="" /></a>
+        <a href="https://www.linkedin.com/in/malsha-pabodani-jayasinghe-64a805292/" className='lg:w-1/3 hover:shadow-lg'><img src="Malsha.png"  alt="" /></a>
+        <a href="https://www.linkedin.com/in/monalisuriarachchi/" className='lg:w-1/3 hover:shadow-lg'><img src="Monali.png"  alt="" /></a>
+        <a href="https://www.linkedin.com/in/sadinsa-welagedara-68013b295/" className='lg:w-1/3 hover:shadow-lg'><img src="Sadinsa.png"  alt="" /></a>
       </div>
 
     </div>
